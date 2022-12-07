159620025d8f6363c3d3d1f55092bd7ea3662336
Xavier Hillman <xhillman13@gmail.com>
2022-12-06 21:56:38 -0500

    add navigation after note save

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
 import { NoteData, Tag } from "../App";
 import {v4 as uuidV4} from 'uuid';
@@ -18,6 +18,8 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
 
+  const navigateHome = useNavigate();
+
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
 
@@ -26,6 +28,8 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
       markdown: markdownRef.current!.value,
       tags: selectedTags,
     })
+
+    navigateHome('..');
   }
   
   return (
