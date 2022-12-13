c814dee731ca8377b1fd6b3ba553286e1d37d34e
Xavier Hillman <xhillman13@gmail.com>
2022-12-12 23:36:13 -0500

    add note title and tag filter form

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,9 +1,16 @@
-import { Button, Col, Row, Stack } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import ReactSelect from "react-select";
+import { Tag } from "../App";
 
-export function NoteList() {
+type NoteListProps = {
+  availableTags: Tag[]
+}
 
-  
+export function NoteList({availableTags}: NoteListProps) {
+
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
 
   return (
     <>
@@ -14,9 +21,38 @@ export function NoteList() {
             <Link to='/new'>
               <Button variant='primary'>New Note</Button>
             </Link>
+            <Button variant='outline-secondary'>Edit Tags</Button>
           </Stack>
         </Col>
       </Row>
+      <Form>
+        <Row className='mb-4'>
+          <Col>
+            <Form.Group controlId='title'>
+              <Form.Label>Title</Form.Label>
+              <Form.Control type='text' placeholder='Name your note...' />
+            </Form.Group>
+          </Col>
+          <Col>
+          <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <ReactSelect
+              value={selectedTags.map(tag => { 
+                return { label: tag.label, value: tag.id }
+              })} 
+              options={availableTags.map(tag => {
+                return { label: tag.label, value: tag.id }
+              })}
+              onChange={tags => {
+                setSelectedTags(tags.map(tag => {
+                  return { id: tag.value, label: tag.label }
+                }))
+              }}
+              isMulti />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
     </>
   )
 }
